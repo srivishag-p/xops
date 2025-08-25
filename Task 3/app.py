ba1b01e148b1187492d7b3198553e77a5d45cdeb
srivishag-p <srivishagp@celcomsolutions.com>
2025-08-25 12:26:00 +0530

    Task 3 update 3

--- Task 3/app.py
+++ Task 3/app.py
@@ -1,15 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Flask CI/CD from GitHub Actions to Amazon Linux 2!"
+    return render_template("index.html")
 
 @app.route("/api/health")
 def health_check():
     return jsonify(status="ok", message="App is running fine ✅")
 
 if __name__ == "__main__":
-    # Run on port 5000 instead of 80
     app.run(host="0.0.0.0", port=5000, debug=True)
